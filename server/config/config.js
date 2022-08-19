5d1f30771373fd196a06bd0fbc26df9f43bc1051
Fernando Brizuela <ffba97@gmail.com>
2022-08-19 01:10:01 -0400

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+
+
+// =======================================
+// PUERTO
+// =======================================
+// es igual al process.env.PORT ó  3000
+process.env.PORT = process.env.PORT || 3000;
+
