5c566ac7254d6036596afadf8e43d165679ec7f9
Fernando Brizuela <ffba97@gmail.com>
2022-08-31 12:25:43 -0400

    preparado para subir 2

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ environment = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (environment === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
+// if (environment === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
     urlDB = 'mongodb+srv://fernando:12345@cafe.uby8kn7.mongodb.net/?retryWrites=true&w=majority';
-}
+// }
 
 process.env.URLDB = urlDB;
